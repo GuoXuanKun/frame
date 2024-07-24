55fb01c8d474454d3ae8763ba0e00f59be58c304
GuoXuanKun <1925918787@qq.com>
2024-07-24 22:56:54 +0800

    feat(DaoInterface): 定义接口

---
+++ MybatisProject01/src/main/java/com/gmgx/dao/IStudentDao.java
@@ -0,0 +1,42 @@
+package com.gmgx.dao;
+
+import com.gmgx.entity.Student;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface IStudentDao {
+    public List<Student> queryAllStudent();
+
+    public Student queryStudentBySno(Integer sno);
+
+    /**
+     *
+     * @param stu
+     * @return
+     */
+    public boolean updateStudentAge(Student stu);
+
+    /**
+     * 用 模糊查询 查询学生
+     * @param sname
+     * @return
+     */
+    public List<Student> queryStudentByLikeSname(String sname);
+
+    /**
+     * 分页查询学生
+     * @param startNum
+     * @param pageSize
+     * @return
+     */
+    public List<Student> queryStudentByPage(@Param("startNum") Integer startNum , @Param("pageSize") Integer pageSize);
+
+    /**
+     * 查询学生 排序
+     * @param colName
+     * @param orderType
+     * @return
+     */
+    public List<Student> queryStudentByOrder(@Param("colName") String colName,@Param("orderType") String orderType);
+}
