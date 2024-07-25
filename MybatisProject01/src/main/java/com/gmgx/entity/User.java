0a27f392d0c1bbaba3cd9fc8de6cd4815aabf9e7
GuoXuanKun <1925918787@qq.com>
2024-07-25 14:22:51 +0800

    feat(entity): 新增 User 实体类

---
+++ MybatisProject01/src/main/java/com/gmgx/entity/User.java
@@ -0,0 +1,17 @@
+package com.gmgx.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private Integer userId;
+    private String userName;
+    private String userPwd;
+    private String userAddr;
+
+}
