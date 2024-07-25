8a9e383b91a6e316ed6a3bb430ae644cfd85970f
GuoXuanKun <1925918787@qq.com>
2024-07-25 22:37:34 +0800

    feat(dto): 增加 ClassroomDto.java

---
+++ MybatisProject01/src/main/java/com/gmgx/dto/ClassroomDto.java
@@ -0,0 +1,18 @@
+package com.gmgx.dto;
+
+import java.util.Date;
+
+public class ClassroomDto {
+
+    private Integer cno;
+    private String cname;
+    private String addr;
+
+    private Integer sno;
+    private String sname;
+    private String password;
+    private String sex;
+    private Integer age;
+    private Date birthday;
+
+}
