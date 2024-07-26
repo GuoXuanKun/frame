6bf4177e30922c41c5b921075a3dcd16a3aac0c5
GuoXuanKun <1925918787@qq.com>
2024-07-26 13:31:20 +0800

    feat(Brand.*): 新增根据 品牌id 查询品牌描述

--- MybatisElecBusiness/src/main/java/com/gmgx/dao/IBrandDao.java
+++ MybatisElecBusiness/src/main/java/com/gmgx/dao/IBrandDao.java
@@ -4,9 +4,18 @@ import com.gmgx.entity.Brand;
 
 import java.util.List;
 
-/**
- * 查询所有品牌
- */
+
 public interface IBrandDao {
+
+    /**
+     * 查询所有品牌
+     */
     public List<Brand> queryAllBrand();
+
+    /**
+     * 根据 品牌 id 查询品牌描述
+     * @param id
+     * @return
+     */
+    public String queryDescription(int id);
 }
