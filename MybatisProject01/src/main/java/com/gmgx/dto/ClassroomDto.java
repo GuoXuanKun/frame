aa6fc93a69eaa85e01f35f8a942c330989b77ccb
GuoXuanKun <1925918787@qq.com>
2024-07-26 09:45:58 +0800

    feat(dto): 增加 ClassroomDto.java 增加注解

--- MybatisProject01/src/main/java/com/gmgx/dto/ClassroomDto.java
+++ MybatisProject01/src/main/java/com/gmgx/dto/ClassroomDto.java
@@ -1,7 +1,15 @@
 package com.gmgx.dto;
 
-import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ClassroomDto {
 
     private Integer cno;
