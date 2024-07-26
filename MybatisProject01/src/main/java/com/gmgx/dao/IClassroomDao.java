6e15b957f57bae92010bd72d0582a7e50c3642c3
GuoXuanKun <1925918787@qq.com>
2024-07-26 09:50:58 +0800

    feat(IClassroomDao.java): 增加 IClassroomDao.java

---
+++ MybatisProject01/src/main/java/com/gmgx/dao/IClassroomDao.java
@@ -0,0 +1,12 @@
+package com.gmgx.dao;
+
+import com.gmgx.dto.ClassroomDto;
+import com.gmgx.entity.Classroom;
+
+import java.util.List;
+
+public interface IClassroomDao {
+
+    public List<ClassroomDto> queryAllClassroomAndStudent();
+    public List<Classroom> queryAllClassroomAndStudent2();
+}
