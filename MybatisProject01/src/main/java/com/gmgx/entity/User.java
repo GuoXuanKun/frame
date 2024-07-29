ada9a335fc42bb3feb09b7a66878da45b919581e
GuoXuanKun <1925918787@qq.com>
2024-07-29 15:23:03 +0800

    refactor(entity): User 实体类 新增 订单列表

--- MybatisProject01/src/main/java/com/gmgx/entity/User.java
+++ MybatisProject01/src/main/java/com/gmgx/entity/User.java
@@ -3,15 +3,20 @@ package com.gmgx.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class User {
 
     private Integer userId;
     private String userName;
     private String userPwd;
     private String userAddr;
+    private List<Order> orderList;
 
 }
