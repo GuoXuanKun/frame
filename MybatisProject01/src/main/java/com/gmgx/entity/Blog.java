62c2411e554cfef60dc469ae75d5112016f3f56f
GuoXuanKun <1925918787@qq.com>
2024-07-30 18:34:39 +0800

    fix(entity):  Blog 实体类 实现 Serializable 接口 ...
    实现了 Serializable 接口，使它的对象才能被序列化。

--- MybatisProject01/src/main/java/com/gmgx/entity/Blog.java
+++ MybatisProject01/src/main/java/com/gmgx/entity/Blog.java
@@ -5,11 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Blog {
+public class Blog implements Serializable {
     private Integer blogId;
     private String blogTitle;
     private String blogContent;
