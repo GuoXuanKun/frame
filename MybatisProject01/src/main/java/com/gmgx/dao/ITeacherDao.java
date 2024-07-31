edb4d75a42e8cfbc25af4dc31fa582bf7af33fca
GuoXuanKun <1925918787@qq.com>
2024-07-31 21:15:23 +0800

    refactor(dao): 新增 deleteTeacher modifyTeacher , 一对多配置 , 一对一配置

--- MybatisProject01/src/main/java/com/gmgx/dao/ITeacherDao.java
+++ MybatisProject01/src/main/java/com/gmgx/dao/ITeacherDao.java
@@ -2,10 +2,7 @@ package com.gmgx.dao;
 
 
 import com.gmgx.entity.Teacher;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -22,12 +19,37 @@ public interface ITeacherDao {
 
     public boolean addTeacher(Teacher teacher);
 
-    @Update("update t_teacher set  tname = #{tname} , sex=#{sex} where tno=#{tno}  ")
-    public boolean updateTeacher(Teacher teacher);
+    @Delete(" delete from t_teacher where tno=#{tno}  ")
+    public  boolean deleteTeacher(Integer tno);
 
-    @Delete("delete from t_teacher where tno=#{tno} ")
-    public boolean deleteTeacher(Teacher teacher);
+    @Update(" update t_teacher set tname =#{tname} where tno  =#{tno} ")
+    public  boolean modifyTeacher(@Param("tno")Integer tno,@Param("tname")String tname);
 
+
+
+
+    // 一对多的配置
+    @Select("  select tno,tname,sex  from t_teacher where tno  =#{tno}  ")
+    @Results({
+            @Result(id = true,property = "tno",column = "tno"),
+            @Result(property = "tname",column = "tname"),
+            @Result(property = "sex",column = "sex"),
+            @Result(property = "myStuList" ,column = "tno",  // javaType = List.class,
+                    many = @Many(select = "com.gmgx.dao.IStudentDao2.queryStudentByTno")
+            )
+    })
+    public Teacher queryTeacherAndStudentsByTno(Integer tno);
+
+
+    @Select(" select tno,tname,sex,cno mycno  from t_teacher where tno  =#{tno}  ")
+    @Results({
+            @Result(id = true,property = "tno",column = "tno"),// 主键:(一定要写)，其他键如果列名和属性名一致，可以不写
+            @Result(property = "myClassroom" ,column = "mycno",// javaType = Classroom.class,可以省略
+                    one = @One(select = "com.gmgx.dao.IClassroomDao.queryClassroomByCno")// 一对一的配置
+            )
+
+    })
+    public Teacher queryTeacherAndClassroomByTno(Integer tno);
 
 
 
