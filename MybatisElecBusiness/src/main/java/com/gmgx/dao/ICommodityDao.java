29fa8b79d5048b20333e18ffb2b7b58e454d16ce
GuoXuanKun <1925918787@qq.com>
2024-07-31 10:08:36 +0800

    feat(dao): 添加 根据商品名称查询商品

--- MybatisElecBusiness/src/main/java/com/gmgx/dao/ICommodityDao.java
+++ MybatisElecBusiness/src/main/java/com/gmgx/dao/ICommodityDao.java
@@ -1,4 +1,10 @@
 package com.gmgx.dao;
 
+import com.gmgx.entity.Commodity;
+
+import java.util.List;
+
 public interface ICommodityDao {
+
+    public List<Commodity> queryAllCommodityByCommodityName(String commodityName);
 }
