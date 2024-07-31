e7d9527898f80e6a8ea8e4575828cf05ee04de4f
GuoXuanKun <1925918787@qq.com>
2024-07-31 13:09:58 +0800

    refactor(dao): 查询商品 选择 排序

--- MybatisElecBusiness/src/main/java/com/gmgx/dao/ICommodityDao.java
+++ MybatisElecBusiness/src/main/java/com/gmgx/dao/ICommodityDao.java
@@ -1,6 +1,7 @@
 package com.gmgx.dao;
 
 import com.gmgx.entity.Commodity;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -12,4 +13,12 @@ public interface ICommodityDao {
      * @return
      */
     public List<Commodity> queryAllCommodityByCommodityName(String commodityName);
+
+    /**
+     * 查询商品 选择 排序
+     * @param choose
+     * @param way
+     * @return
+     */
+    public List<Commodity> queryAllCommodityByChoose(@Param("choose") String choose,@Param("way") String way);
 }
