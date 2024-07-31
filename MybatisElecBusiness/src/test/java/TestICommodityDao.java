8ee618722653b128cb20f5e7b767e602574d94a3
GuoXuanKun <1925918787@qq.com>
2024-07-31 10:10:14 +0800

    test(ICommodityDao.java): 测试 根据商品名称查询商品

---
+++ MybatisElecBusiness/src/test/java/TestICommodityDao.java
@@ -0,0 +1,46 @@
+import com.gmgx.dao.IBrandDao;
+import com.gmgx.dao.ICommodityDao;
+import com.gmgx.entity.Brand;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestICommodityDao {
+    SqlSession session   =null;
+    ICommodityDao commodityDao =null;
+
+    @Before
+    public  void beforeMethod(){
+        // 1 找到 sqlmapConfig.xml
+        String name  ="SqlMapConfig.xml";
+        // 实际上 要从工程角度出发，当前这个文件是在工程的相对位置 得到它的io流
+        InputStream is  = TestICommodityDao.class.getClassLoader().getResourceAsStream(name);
+
+        // 2 把它加载到 sql会话工厂中 帮你解析(给工厂 加载 了 装备（指导书）)
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+
+        // 3 通过会话工程 ，获得一个 sql会话
+        session   =  sessionFactory.openSession();
+
+        commodityDao  =  session.getMapper(ICommodityDao.class);
+    }
+    @After
+    public void afterMethod(){
+
+        // 6 关闭资源
+        session.close();
+
+    }
+
+    @Test
+    public void testQueryAllCommodityByCommodityName(){
+        System.out.println(commodityDao.queryAllCommodityByCommodityName("苹果"));
+    }
+}
