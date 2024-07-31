9fc81d79fe0afc716710f60b4f51bbaa317e37d9
GuoXuanKun <1925918787@qq.com>
2024-07-31 20:57:24 +0800

    feat(dao): 新增 queryClassroomByCno

--- MybatisProject01/src/main/java/com/gmgx/dao/IClassroomDao.java
+++ MybatisProject01/src/main/java/com/gmgx/dao/IClassroomDao.java
@@ -9,4 +9,8 @@ public interface IClassroomDao {
 
     public List<ClassroomDto> queryAllClassroomAndStudent();
     public List<Classroom> queryAllClassroomAndStudent2();
+
+    public Classroom   queryClassroomByCno(Integer cno);
+
+
 }
