a1bdd0e9e5d02d52c17edd896f50509528605f3e
GuoXuanKun <1925918787@qq.com>
2024-08-01 15:46:36 +0800

    test: 测试 单例 和 原型 的对象的 创建过程

--- SpringProject01/src/test/java/TestGetBean.java
+++ SpringProject01/src/test/java/TestGetBean.java
@@ -11,10 +11,23 @@ public class TestGetBean {
         String fileName  = "applicationContext.xml";
 
         ClassPathXmlApplicationContext applicationContext =    new   ClassPathXmlApplicationContext(fileName);
+        System.out.println("======= 调用getBean之前=========");
 
-        Student stu1  = (Student) applicationContext.getBean("stu1");
+        //单例模式下：getBean去容器中找 对象 原型模式：只会让容器创建出一个新对象，给我们用
+        Student stu1  = (Student) applicationContext.getBean("stu");
+        Student stu2  = (Student) applicationContext.getBean("stu");
+        System.out.println("======= 调用getBean之后=========");
 
+//        Student stu2  = (Student) applicationContext.getBean("stu");
+        System.out.println("======= 使用这个对象之前=========");
         System.out.println(stu1);
+        System.out.println(stu2);
+        System.out.println(stu1==stu2);
+        System.out.println("======= 使用这个对象之后=========");
+        System.out.println("======= 关闭上下文对象之前=========");
+        applicationContext.close();
+        System.out.println("======= 关闭上下文对象之后=========");
+
 
     }
 }
