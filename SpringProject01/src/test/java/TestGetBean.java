7cbb4a20d51740b7aa2ddcc52869b2a3cbe97af6
GuoXuanKun <1925918787@qq.com>
2024-08-01 11:50:07 +0800

    test: 加载配置

--- SpringProject01/src/test/java/TestGetBean.java
+++ SpringProject01/src/test/java/TestGetBean.java
@@ -1,2 +1,20 @@
+import com.gmgx.entity.Student;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class TestGetBean {
+
+    @Test
+    public void test01(){
+
+
+        String fileName  = "applicationContext.xml";
+
+        ClassPathXmlApplicationContext applicationContext =    new   ClassPathXmlApplicationContext(fileName);
+
+        Student stu1  = (Student) applicationContext.getBean("stu1");
+
+        System.out.println(stu1);
+
+    }
 }
