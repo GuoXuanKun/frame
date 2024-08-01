6a51f004bedd3944f09a10bccf29f976240529c5
GuoXuanKun <1925918787@qq.com>
2024-08-01 19:36:28 +0800

    init(entity): 创建 Teacher.java 实体类

---
+++ SpringProject01/src/main/java/com/gmgx/entity/Teacher.java
@@ -0,0 +1,16 @@
+package com.gmgx.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Teacher {
+
+    private Integer tno;
+    private  String tname;
+    private Classroom classroom;
+
+}
