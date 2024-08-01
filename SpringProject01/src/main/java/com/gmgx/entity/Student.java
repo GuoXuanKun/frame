c2c34294470aa4f57b45f04999f99598e1cc6718
GuoXuanKun <1925918787@qq.com>
2024-08-01 15:45:06 +0800

    feat(entity): Student 实体类 创建 构造方法 , 初始化方法 , 销毁方法

--- SpringProject01/src/main/java/com/gmgx/entity/Student.java
+++ SpringProject01/src/main/java/com/gmgx/entity/Student.java
@@ -6,10 +6,30 @@ import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
+
 public class Student {
 
     private Integer sno;
     private String sname;
+
+    public Student() {
+        System.out.println("Student的无参构造");
+
+    }
+
+
+
+    //    init-method=""
+    public void initMethod(){
+        System.out.println("初始化Student对象");
+
+    }
+    //    destroy-method=""
+    public void destroyMethod(){
+
+        System.out.println("销毁Student对象");
+
+
+    }
 
 }
