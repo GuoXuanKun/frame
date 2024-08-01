0c47a7f3f2b31b802bb87a80d677a56f3d5355a1
GuoXuanKun <1925918787@qq.com>
2024-08-01 11:45:59 +0800

    init(entity): 添加 Student.java 实体类

---
+++ SpringProject01/src/main/java/com/gmgx/entity/Student.java
@@ -0,0 +1,15 @@
+package com.gmgx.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    private Integer sno;
+    private String sname;
+
+}
