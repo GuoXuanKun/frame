faa3dde7ac2f9db3ea1c18644ec7c7a2d2755a6d
GuoXuanKun <1925918787@qq.com>
2024-08-03 19:47:48 +0800

    init(controller): 创建 StudentServlet

---
+++ SpringMVCProject01/src/main/java/com/gmgx/controller/StudentServlet.java
@@ -0,0 +1,25 @@
+package com.gmgx.controller;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class StudentServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        config.getInitParameter("name");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("student doPost");
+    }
+}
