8a34f46b1d27c9aaf503d98c6c2f63626616891c
GuoXuanKun <1925918787@qq.com>
2024-08-03 20:35:50 +0800

    feat(controller):  StudentController.java 添加 模拟登录成功 调转到 main 页面

--- SpringMVCProject01/src/main/java/com/gmgx/controller/StudentController.java
+++ SpringMVCProject01/src/main/java/com/gmgx/controller/StudentController.java
@@ -6,8 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("student")
 public class StudentController {
+
     @RequestMapping("login")
     public void login(){
         System.out.println("login");
+    }
+
+    @RequestMapping("login2")
+    public String login2(){
+
+        System.out.println("login方法");
+        return "main";
     }
 }
