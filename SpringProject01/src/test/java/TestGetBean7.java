37b1eef174c97cbdae0c64c2e2fa005c02e5feb5
GuoXuanKun <1925918787@qq.com>
2024-08-04 16:08:07 +0800

    test: 自动注入 - byType ...
    byType
    根据类型自动注入
    优点；不用考虑名字 属性名和id不一致也没关系
    缺点：一个Spring容器中（配置文件中），只能有一个这种类型的bean，否则直接报错（编译时异常）

---
+++ SpringProject01/src/test/java/TestGetBean7.java
@@ -0,0 +1,34 @@
+import com.gmgx.entity.Teacher;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestGetBean7 {
+
+    ClassPathXmlApplicationContext applicationContext= null;
+
+    @Before
+    public void beforeMethod(){
+        String fileName  ="applicationContext7.xml";
+        applicationContext  = new ClassPathXmlApplicationContext(fileName);
+
+    }
+    @After
+    public void  afterMethod(){
+        applicationContext.close();
+    }
+
+
+    @Test
+    public void test1(){
+    /*  */
+      Teacher t1  = (Teacher) applicationContext.getBean("t1");
+        System.out.println(t1);
+
+    }
+
+
+
+
+}
