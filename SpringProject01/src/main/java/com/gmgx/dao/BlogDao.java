88c66c58fa799a433a668595b0f0e05e221bf2ab
GuoXuanKun <1925918787@qq.com>
2024-08-05 13:15:26 +0800

    init(dao): 创建 BlogDao

---
+++ SpringProject01/src/main/java/com/gmgx/dao/BlogDao.java
@@ -0,0 +1,11 @@
+package com.gmgx.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BlogDao {
+
+    public  void login(){
+        System.out.println("blog dao login");
+    }
+}
