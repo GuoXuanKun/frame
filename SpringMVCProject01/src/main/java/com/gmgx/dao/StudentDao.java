34998a003b9ae4b74a06083921c30ffa38e6bb91
GuoXuanKun <1925918787@qq.com>
2024-08-05 20:00:22 +0800

    fix(dao): 完善 学生登录功能

--- SpringMVCProject01/src/main/java/com/gmgx/dao/StudentDao.java
+++ SpringMVCProject01/src/main/java/com/gmgx/dao/StudentDao.java
@@ -12,65 +12,82 @@ import java.util.List;
 @Repository
 public class StudentDao {
 
-@Autowired
-private JDBCUtils jdbcUtils;
+    @Autowired
+    private JDBCUtils jdbcUtils;
 
-public List<Student> queryAllStudent(){
-  List<Student> list  = new ArrayList<>();
+    public List<Student> queryAllStudent() {
+        List<Student> list = new ArrayList<>();
 
-    ResultSet rs  = jdbcUtils.doQuery("select sno,sname,password from t_student");
-   try {
-       while (rs.next()){
-           Student student  =     new Student();
-           student.setSno(rs.getInt("sno"));
-           student.setSname(rs.getString("sname"));
-           student.setPassword(rs.getString("password"));
-            list.add(student);
-       }
+        ResultSet rs = jdbcUtils.doQuery("select sno,sname,password from t_student");
+        try {
+            while (rs.next()) {
+                Student student = new Student();
+                student.setSno(rs.getInt("sno"));
+                student.setSname(rs.getString("sname"));
+                student.setPassword(rs.getString("password"));
+                list.add(student);
+            }
 
 
-
-   }catch (Exception e){
-       e.printStackTrace();
-   }finally {
-       jdbcUtils.doClose(rs);
-   }
-    return  list;
-}
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            jdbcUtils.doClose(rs);
+        }
+        return list;
+    }
 
 
     /**
      * 学生注册
+     *
      * @param student
      * @return
      */
-    public boolean register(Student student){
-        List<Student> list  = new ArrayList<>();
+    public boolean register(Student student) {
+        List<Student> list = new ArrayList<>();
 
-        String sql   ="insert into t_student(sname,password)values(?,?)";
-        int newSno  = jdbcUtils.doAddObject(sql,student.getSname(),student.getPassword());
+        String sql = "insert into t_student(sname,password)values(?,?)";
+        int newSno = jdbcUtils.doAddObject(sql, student.getSname(), student.getPassword());
 
         student.setSno(newSno);
 
-        return newSno>0;
+        return newSno > 0;
 
     }
 
     /**
      * 学生登录
+     *
      * @param sno
      * @param password
      * @return
      */
-    public boolean login(int sno,String password){
+    public List<Student> login(int sno, String password) {
+        List<Student> list = new ArrayList<>();
 
-        String sql   ="select * from t_student where sno=? and password = ?";
-        int result  = jdbcUtils.doAddObject(sql,sno,password);
+        String sql = "select * from t_student where sno=? and password = ?";
+        ResultSet rs = jdbcUtils.doQuery(sql, sno, password);
+
+        try {
+            while (rs.next()) {
+                Student student = new Student();
+                student.setSno(rs.getInt("sno"));
+                student.setSname(rs.getString("sname"));
+                student.setPassword(rs.getString("password"));
+                list.add(student);
+            }
 
 
-        return result>0;
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            jdbcUtils.doClose(rs);
+        }
+        return list;
     }
 
-
 }
+
+
+
