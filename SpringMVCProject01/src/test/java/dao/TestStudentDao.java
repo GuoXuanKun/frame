88edfed786f1fe00ef4d1bdc3aa35464d98d9b8f
GuoXuanKun <1925918787@qq.com>
2024-08-05 20:01:36 +0800

    test(dao) : 测试 学生登录功能

--- SpringMVCProject01/src/test/java/dao/TestStudentDao.java
+++ SpringMVCProject01/src/test/java/dao/TestStudentDao.java
@@ -16,4 +16,14 @@ public class TestStudentDao {
 
     }
 
+    @Test
+    public  void testStudentLogin(){
+
+        ClassPathXmlApplicationContext applicationContext  = new ClassPathXmlApplicationContext("applicationContext.xml");
+        // StudentDao studentDao  =  new StudentDao(); 不能直接new，因为 JDBCUtils对象通过 IOC DI 方式获得的，并非自己new，
+        StudentDao studentDao  =       applicationContext.getBean(StudentDao.class);
+        System.out.println(studentDao.login(11,"123"));
+
+    }
+
 }
