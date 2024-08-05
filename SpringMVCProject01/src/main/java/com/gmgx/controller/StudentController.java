cba16ac5b6f17544e61321223b0e1e2602394b51
GuoXuanKun <1925918787@qq.com>
2024-08-05 18:54:25 +0800

    feat(controller): 添加 学生注册

--- SpringMVCProject01/src/main/java/com/gmgx/controller/StudentController.java
+++ SpringMVCProject01/src/main/java/com/gmgx/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.gmgx.controller;
 
 import com.gmgx.entity.Student;
+import com.gmgx.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,6 +10,33 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("student")
 public class StudentController {
+
+    @Autowired
+    private IStudentService studentService;
+
+
+    @RequestMapping("register")
+    public ModelAndView regisger(Student student){
+        System.out.println(student);
+        ModelAndView mv  = new ModelAndView();
+        boolean flag  =  studentService.register(student);
+
+        if(flag){
+            mv.addObject("msg","注册成功，您的学号为："+student.getSno());
+            mv.setViewName("main");
+        }else{
+            mv.addObject("msg","注册失败");
+            mv.setViewName("register");
+        }
+
+        return  mv;
+
+    }
+
+
+
+
+
     @RequestMapping("login")
     public void login(){
 
@@ -45,20 +74,20 @@ public class StudentController {
     }
 
 
-    @RequestMapping("register")
-    public ModelAndView register(Student student){
-
-
-        System.out.println("register:stu=" +student);
-
-
-        ModelAndView mv   = new   ModelAndView();
-        // 相当于request.setAttribute();
-        mv.addObject("msg","成功");
-        /* jsp 文件名 （不用后缀）  */
-        mv.setViewName("main");
-
-        return mv;
-    }
+//    @RequestMapping("register")
+//    public ModelAndView register(Student student){
+//
+//
+//        System.out.println("register:stu=" +student);
+//
+//
+//        ModelAndView mv   = new   ModelAndView();
+//        // 相当于request.setAttribute();
+//        mv.addObject("msg","成功");
+//        /* jsp 文件名 （不用后缀）  */
+//        mv.setViewName("main");
+//
+//        return mv;
+//    }
 
 }
