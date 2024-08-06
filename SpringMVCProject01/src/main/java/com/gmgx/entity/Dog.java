aa770856bcf41ca0b7d2e16937277813c1be0f3c
GuoXuanKun <1925918787@qq.com>
2024-08-06 15:30:30 +0800

    init(entity) : 新增 Dog 实体类

---
+++ SpringMVCProject01/src/main/java/com/gmgx/entity/Dog.java
@@ -0,0 +1,18 @@
+package com.gmgx.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Dog {
+
+    private  Integer did;
+    private String dname;
+    private String sex;
+
+
+
+}
