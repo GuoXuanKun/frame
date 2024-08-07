5dd878812e033b669eb7479a1e80a1ded3d0fc4f
GuoXuanKun <1925918787@qq.com>
2024-08-07 21:05:58 +0800

    feat(controller):  StudentController 增加 queryAllStudent() ...
    查询所有学生 方法

--- SSMProject01/src/main/java/com/gmgx/controller/StudentController.java
+++ SSMProject01/src/main/java/com/gmgx/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.gmgx.controller;
 
+import com.gmgx.entity.Student;
 import com.gmgx.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("student")
@@ -11,4 +15,14 @@ public class StudentController {
 
     @Autowired
     private IStudentService studentService;
+
+    @RequestMapping("queryAllStudent")
+    public ModelAndView queryAllStudent(){
+        ModelAndView mv = new ModelAndView();
+        List<Student> slist = studentService.queryAllStudent();
+        mv.addObject(slist);
+        mv.setViewName("queryAllStudent");
+
+        return mv;
+    }
 }
