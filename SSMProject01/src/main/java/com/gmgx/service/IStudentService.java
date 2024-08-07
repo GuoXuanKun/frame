1f01167cd730a2a05e8ecf101ee677e8f9b5e2e6
GuoXuanKun <1925918787@qq.com>
2024-08-07 20:45:35 +0800

    feat(service): 定义 查询所有学生 方法

--- SSMProject01/src/main/java/com/gmgx/service/IStudentService.java
+++ SSMProject01/src/main/java/com/gmgx/service/IStudentService.java
@@ -1,4 +1,14 @@
 package com.gmgx.service;
 
+import com.gmgx.entity.Student;
+
+import java.util.List;
+
 public interface IStudentService {
+
+    /**
+     * 查询所有学生
+     * @return
+     */
+    public List<Student> queryAllStudent();
 }
