5d5ae690497fc74402c324dc114576ce0ff167c4
GuoXuanKun <1925918787@qq.com>
2024-08-07 10:53:19 +0800

    init(controller) : 创建 AjaxStudentContrller

---
+++ SpringMVCProject01/src/main/java/com/gmgx/controller/AjaxStudentContrller.java
@@ -0,0 +1,24 @@
+package com.gmgx.controller;
+
+import com.gmgx.entity.Student;
+import com.gmgx.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("ajaxStudent")
+public class AjaxStudentContrller {
+
+    @Autowired
+    private IStudentService studentService;
+
+
+
+
+
+}
