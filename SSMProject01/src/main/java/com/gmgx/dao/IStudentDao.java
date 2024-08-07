ee76f9a893a04c63ab271c893609d1b5f65cf4ae
GuoXuanKun <1925918787@qq.com>
2024-08-07 20:38:04 +0800

    feat(dao): 增加 queryAllStudent()

--- SSMProject01/src/main/java/com/gmgx/dao/IStudentDao.java
+++ SSMProject01/src/main/java/com/gmgx/dao/IStudentDao.java
@@ -1,4 +1,15 @@
 package com.gmgx.dao;
 
+import com.gmgx.entity.Student;
+
+import java.util.List;
+
 public interface IStudentDao {
+
+    /**
+     * 查询所有学生
+     * @return
+     */
+    public List<Student> queryAllStudent();
+
 }
