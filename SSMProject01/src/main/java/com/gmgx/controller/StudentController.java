d5a6e5a831c05d2f7b27f327b3842854b13ead34
GuoXuanKun <1925918787@qq.com>
2024-08-07 21:00:22 +0800

    refactor(controller):  StudentController 添加 IStudentService 类型的属性

--- SSMProject01/src/main/java/com/gmgx/controller/StudentController.java
+++ SSMProject01/src/main/java/com/gmgx/controller/StudentController.java
@@ -1,9 +1,14 @@
 package com.gmgx.controller;
 
+import com.gmgx.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("student")
 public class StudentController {
+
+    @Autowired
+    private IStudentService studentService;
 }
