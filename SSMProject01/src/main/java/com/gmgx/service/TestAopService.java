aeebeeb4c5c13f30e3e244f640e469844390eb73
GuoXuanKun <1925918787@qq.com>
2024-08-08 20:52:51 +0800

    init(service): 创一个目标类及方法（TestAopService类 ，为了测试用的）

---
+++ SSMProject01/src/main/java/com/gmgx/service/TestAopService.java
@@ -0,0 +1,12 @@
+package com.gmgx.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestAopService {
+
+
+    public  void  login(){
+        System.out.println("我准备登录le 。。。。");
+    }
+}
