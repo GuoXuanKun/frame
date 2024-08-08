59e9ecc431d62e15648ecbf27f68739f2fb60e78
GuoXuanKun <1925918787@qq.com>
2024-08-08 23:55:45 +0800

    init(service): 写为了 测试 而写的方法

--- SSMProject01/src/main/java/com/gmgx/service/TestAopService.java
+++ SSMProject01/src/main/java/com/gmgx/service/TestAopService.java
@@ -5,8 +5,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestAopService {
 
+    public  String  login(){
 
-    public  void  login(){
         System.out.println("我准备登录le 。。。。");
+        int i =1/0;
+        return "我做到了~~~";
     }
+
+    public  boolean  addMsg(String msg){
+
+        System.out.println("添加数据"+msg);
+//        int i =1/0;
+        return true;
+    }
+
+
 }
