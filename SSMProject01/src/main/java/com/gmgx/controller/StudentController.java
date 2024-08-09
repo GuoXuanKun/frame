b822d80783a078367fdd12ae0f1c26b53f02348a
GuoXuanKun <1925918787@qq.com>
2024-08-09 10:44:40 +0800

    feat(controller): 新增 login()

--- SSMProject01/src/main/java/com/gmgx/controller/StudentController.java
+++ SSMProject01/src/main/java/com/gmgx/controller/StudentController.java
@@ -5,16 +5,28 @@ import com.gmgx.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("student")
+@SessionAttributes({"msg","my"})
 public class StudentController {
 
     @Autowired
     private IStudentService studentService;
+
+
+    @RequestMapping("login")
+    public ModelAndView login(){
+        ModelAndView mv   = new ModelAndView();
+        mv.addObject("my","zhw");
+        return  mv;
+    }
+
+
 
     @RequestMapping("queryAllStudent")
     public ModelAndView queryAllStudent(){
