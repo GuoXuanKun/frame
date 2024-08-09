10a55487dce2b8bdfdc265f8c71d2fe02f938c2b
GuoXuanKun <1925918787@qq.com>
2024-08-09 13:35:05 +0800

    refactor(service test): 调整

--- SSMProject01/src/main/java/com/gmgx/service/TestAopService.java
+++ SSMProject01/src/main/java/com/gmgx/service/TestAopService.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TestAopService {
 
+
     public  String  login(){
 
         System.out.println("我准备登录le 。。。。");
-        int i =1/0;
+       // int i =1/0;
         return "我做到了~~~";
     }
 
--- SSMProject01/src/test/java/com/gmgx/service/TestStudentService.java
+++ SSMProject01/src/test/java/com/gmgx/service/TestStudentService.java
@@ -1,5 +1,6 @@
 package com.gmgx.service;
 
+import com.gmgx.entity.Student;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,7 +13,31 @@ public class TestStudentService {
         ClassPathXmlApplicationContext applicationContext  = new ClassPathXmlApplicationContext("applicationContext.xml");
         IStudentService studentService  =  applicationContext.getBean(IStudentService.class);
         System.out.println(studentService.queryAllStudent());
+        System.out.println(studentService.queryStudentBySno(1));
+        System.out.println(studentService.addStudent(new Student(null,"zhw","123")));
+    }
+
+
+    @Test
+    public void test02(){
+
+        ClassPathXmlApplicationContext applicationContext  = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ITeacherService teacherService  =  applicationContext.getBean(ITeacherService.class);
+        System.out.println(teacherService.queryAllStudent());
 //        System.out.println(studentService.queryStudentBySno(1));
 //        System.out.println(studentService.addStudent(new Student(null,"zhw","123")));
     }
+
+
+    @Test
+    public void test03(){
+
+        ClassPathXmlApplicationContext applicationContext  = new ClassPathXmlApplicationContext("applicationContext.xml");
+        TestAopService testAopService  =  applicationContext.getBean(TestAopService.class);
+        System.out.println(testAopService.login());
+//        System.out.println(studentService.queryStudentBySno(1));
+//        System.out.println(studentService.addStudent(new Student(null,"zhw","123")));
+    }
+
+
 }
