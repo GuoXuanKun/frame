99b3584bd9f17c74464ba54efe846db8ed8c43cb
GuoXuanKun <1925918787@qq.com>
2024-08-10 12:03:51 +0800

    init(interceptor): 新增 拦截器 类 VisitInterceptor

---
+++ SSMProject01/src/main/java/com/gmgx/interceptor/VisitInterceptor.java
@@ -0,0 +1,16 @@
+package com.gmgx.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/* 使用注解方式 配到 Spring容器中 */
+@Component
+public class VisitInterceptor  {  // 实现 处理拦截器 接口 实现三个方法
+
+
+}
