390b36542075b5067734699b0d109844ffd0b21a
GuoXuanKun <1925918787@qq.com>
2024-08-13 23:07:31 +0800

    feat(ui): test02.vue 增加 动态变量

--- vue3/demo01/src/components/test02.vue
+++ vue3/demo01/src/components/test02.vue
@@ -1,8 +1,19 @@
 <script setup>
 
+import {ref} from 'vue'
+
+var msg = ref("gxk");
+
+function changeMsg(){
+    msg.value="666";
+}
+
 </script>
 
 <template>
+
+test02:{{msg}}
+<button v-on:click="changeMsg">点一下</button>
 
 </template>
 
