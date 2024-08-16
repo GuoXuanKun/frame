669e29c5daa0548d5308164387f7f1416d2a9d9a
GuoXuanKun <1925918787@qq.com>
2024-08-16 13:38:54 +0800

    init(Vue3): 配置 路由

--- vue3/demo01/src/App.vue
+++ vue3/demo01/src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
 
-import test02 from './components/test02.vue';
-
 </script>
 
 <template>
 
-<test02></test02>
+<!--  相当于 a标签 点击跳转  -->
+<RouterLink to="home">首页</RouterLink>
+<RouterLink to="news">新闻</RouterLink>|
+<RouterLink to="about">关于</RouterLink>
+
+<RouterView></RouterView>
+<!--   <!—点击 RouterLink  会在  RouterView 显示 对应的组件 -->
 
 </template>
 
--- vue3/demo01/src/main.js
+++ vue3/demo01/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router  from './router'  // 刚刚写的index.js文件 因为router文件夹下有且仅有一个 index.js 可以简写
 
 createApp(App).mount('#app')
