0bf0299e9a350cac88d90efa02e3fc00cc7b1db9
GuoXuanKun <1925918787@qq.com>
2024-08-31 21:27:21 +0800

    feat(controller): 写出基础的增删改查

--- practice/mybatis-plus-demo01/src/main/java/com/xenon/controller/UserController.java
+++ practice/mybatis-plus-demo01/src/main/java/com/xenon/controller/UserController.java
@@ -1,18 +1,74 @@
 package com.xenon.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xenon.entity.User;
+import com.xenon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
- * <p>
- *  前端控制器
- * </p>
- *
- * @author wuyunbin
- * @since 2024-08-31
+ * @author Administrator
  */
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/list")
+    public Object list() {
+        return userService.list();
+    }
+
+    @GetMapping("findByName")
+    public Object findByName(String name) {
+        /*
+        Wrapper<User> wrapper = new LambdaQueryWrapper<>();
+         */
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(User::getName, name);
+        List<User> list = userService.list(wrapper);
+        return list;
+    }
+
+    @GetMapping("findByEmail")
+    public Object findByEmail(String email) {
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(User::getEmail, email);
+        User user = userService.getOne(wrapper);
+        return user;
+    }
+
+    @PostMapping
+    public Object save(@RequestBody User user) {
+        boolean save = userService.save(user);
+        return save;
+    }
+
+    @PutMapping
+    public Object update(@RequestBody User user) {
+        boolean update = userService.updateById(user);
+        return update;
+    }
+
+    @GetMapping
+    public Object getPage(int current,int size){
+        /*
+        2. 页面规格
+        current-当前页码
+        size-每页显示条数
+         */
+        IPage<User> pageInfo = new Page<>(current, size);
+        // 3.查询条件
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.gt(User::getAge,18);
+        // 1.
+        IPage<User> page = userService.page(pageInfo, wrapper);
+        return page;
+    }
 }
