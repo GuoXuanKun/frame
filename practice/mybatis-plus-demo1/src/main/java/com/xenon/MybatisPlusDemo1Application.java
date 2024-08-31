30e156a9917b9774f9b922687ec02161fa23a442
GuoXuanKun <1925918787@qq.com>
2024-08-31 23:51:29 +0800

    训练1

---
+++ practice/mybatis-plus-demo1/src/main/java/com/xenon/MybatisPlusDemo1Application.java
@@ -0,0 +1,13 @@
+package com.xenon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusDemo1Application.class, args);
+    }
+
+}
---
+++ practice/mybatis-plus-demo1/src/main/java/com/xenon/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.xenon.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xenon.entity.User;
+import com.xenon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author wuyunbin
+ * @since 2024-08-31
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("getPage")
+    public Object getPage(int current,int size){
+        IPage<User> pageInfo = new Page<>(current,size);
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.gt(User::getAge,18);
+        IPage<User> page = userService.page(pageInfo, wrapper);
+        return page;
+    }
+}
