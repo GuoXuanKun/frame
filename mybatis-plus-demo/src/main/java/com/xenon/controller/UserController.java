1dcb6e9656aa120d0c1f39f27b7013a2c724fb61
GuoXuanKun <1925918787@qq.com>
2024-08-31 16:50:15 +0800

    refactor(controller): save(@RequestBody User user) ...
    - 实现 添加 user

--- mybatis-plus-demo/src/main/java/com/xenon/controller/UserController.java
+++ mybatis-plus-demo/src/main/java/com/xenon/controller/UserController.java
@@ -4,9 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xenon.entity.User;
 import com.xenon.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,22 +29,25 @@ public class UserController {
     }
 
     @GetMapping("findByName")
-    public Object findByName(String name){
+    public Object findByName(String name) {
         LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(User::getName,name);
+        wrapper.eq(User::getName, name);
         List<User> list = userService.list(wrapper);
         return list;
     }
 
     @GetMapping("findByEmail")
-    public Object findByEmail(String email){
+    public Object findByEmail(String email) {
         LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
-        wrapper.eq(User::getEmail,email);
+        wrapper.eq(User::getEmail, email);
         User user = userService.getOne(wrapper);
         return user;
     }
 
-
+    @PostMapping
+    public Object save(@RequestBody User user) {
+        return userService.save(user);
+    }
 
 
 }
