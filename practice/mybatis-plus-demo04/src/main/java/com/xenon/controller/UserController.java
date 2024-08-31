0f1e0dbcd4e60b9c30a15e8c3a1d7961099bf6ff
GuoXuanKun <1925918787@qq.com>
2024-08-31 23:32:02 +0800

    feat(controller): 新增 分页查询

--- practice/mybatis-plus-demo04/src/main/java/com/xenon/controller/UserController.java
+++ practice/mybatis-plus-demo04/src/main/java/com/xenon/controller/UserController.java
@@ -1,5 +1,12 @@
 package com.xenon.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xenon.entity.User;
+import com.xenon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("getPage")
+    public Object getPage(int current,int size){
+        IPage<User> pageInfo = new Page<>(current,size);
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.gt(User::getAge,18);
+        IPage<User> page = userService.page(pageInfo, wrapper);
+        return page;
+    }
 }
