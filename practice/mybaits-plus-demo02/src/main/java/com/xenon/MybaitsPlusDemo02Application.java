d1077d47bbbb2ce7bb5110c51d02ac7cb81726a0
GuoXuanKun <1925918787@qq.com>
2024-08-31 21:29:33 +0800

    init:创建 SpringBoot 项目

---
+++ practice/mybaits-plus-demo02/src/main/java/com/xenon/MybaitsPlusDemo02Application.java
@@ -0,0 +1,13 @@
+package com.xenon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybaitsPlusDemo02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybaitsPlusDemo02Application.class, args);
+    }
+
+}
