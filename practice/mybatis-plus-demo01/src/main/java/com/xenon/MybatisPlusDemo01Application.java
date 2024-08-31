e2e6351cf11c5bca45cdbb8cbada6c567def1835
GuoXuanKun <1925918787@qq.com>
2024-08-31 17:22:18 +0800

    init(SpringBoot): 新建 SpringBoot 项目

---
+++ practice/mybatis-plus-demo01/src/main/java/com/xenon/MybatisPlusDemo01Application.java
@@ -0,0 +1,13 @@
+package com.xenon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusDemo01Application.class, args);
+    }
+
+}
