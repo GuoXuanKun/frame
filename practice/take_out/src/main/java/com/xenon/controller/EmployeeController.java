2e141bdf5af8101b915cb6ddffd52bb655b76759
GuoXuanKun <1925918787@qq.com>
2024-09-01 15:54:06 +0800

    feat : 新增登录方法

--- practice/take_out/src/main/java/com/xenon/controller/EmployeeController.java
+++ practice/take_out/src/main/java/com/xenon/controller/EmployeeController.java
@@ -1,5 +1,10 @@
 package com.xenon.controller;
 
+import com.xenon.entity.Employee;
+import com.xenon.service.EmployeeService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @author wuyunbin
  * @since 2024-09-01
  */
+@Slf4j
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    @Resource
+    private EmployeeService employeeService;
+
+    public String login(@RequestBody Employee employee){
+        log.info("employee:{}",employee);
+        String token=  employeeService.login(employee);
+        return token;
+    }
 
 }
--- practice/take_out/src/main/java/com/xenon/service/impl/EmployeeServiceImpl.java
+++ practice/take_out/src/main/java/com/xenon/service/impl/EmployeeServiceImpl.java
@@ -1,9 +1,13 @@
 package com.xenon.service.impl;
 
+import cn.hutool.crypto.digest.BCrypt;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xenon.entity.Employee;
 import com.xenon.mapper.EmployeeMapper;
 import com.xenon.service.EmployeeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xenon.utils.JwtUtils;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +20,37 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
+
+    @Resource
+    private JwtUtils jwtUtils;
+    @Override
+    public String login(Employee employee) {
+        //2.构造条件
+        LambdaQueryWrapper<Employee> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Employee::getUsername,employee.getUsername())
+                .or()
+                .eq(Employee::getPhone,employee.getPhone());
+        //1. 根据用户名或者密码查询用户信息
+        Employee employeeInDb = this.getOne(wrapper);
+
+        //3.判断是否查询到用户
+        if(employeeInDb == null){
+            throw new RuntimeException("用户名错误");
+        }
+
+        //4.比较密码
+        if(!BCrypt.checkpw(employee.getPassword(),employeeInDb.getPassword())){
+            throw new RuntimeException("密码错误");
+        }
+        //5.签发token
+
+        String token = jwtUtils.createJwt(
+                employeeInDb.getId(),
+                null,
+                60*60*24*7L,
+                "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+        );
+        return token;
+    }
 
 }
