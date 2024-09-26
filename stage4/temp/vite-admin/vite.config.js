2e8c8922cc388a6c7e78305b903fec823504ff49
GuoXuanKun <1925918787@qq.com>
2024-09-26 14:43:01 +0800

    搭建脚手架 - 修改 vite.config.js : 实现 @ 代替 ../

--- stage4/temp/vite-admin/vite.config.js
+++ stage4/temp/vite-admin/vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
-
+import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),
@@ -14,5 +14,11 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
-
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // 指定 src 文件夹别名，以便快速定位到此文件夹
+      // "~": path.resolve(__dirname, "src")
+    }
+  }
 })
