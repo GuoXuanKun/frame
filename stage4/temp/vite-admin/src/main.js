084159c3721b8736877c1ef6569fcfe0ed56b6e7
GuoXuanKun <1925918787@qq.com>
2024-09-26 14:54:45 +0800

    搭建脚手架 - 安装并引入 pinia (补充)

--- stage4/temp/vite-admin/src/main.js
+++ stage4/temp/vite-admin/src/main.js
@@ -3,6 +3,7 @@ import router from "./router";
 import App from './App.vue'
 import {createPinia} from 'pinia'
 
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
